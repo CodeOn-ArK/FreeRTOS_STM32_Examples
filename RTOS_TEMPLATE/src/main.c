8158fa2a705eb5a7a675b2a946ac063c4dd3b75e
CodeOn-ArK <aarya.codeeka@gmail.com>
2021-06-14 18:58:17 +0530

    standard Template for using RTOS with stdperipheral

---
+++ RTOS_TEMPLATE/src/main.c
@@ -0,0 +1,19 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  CodeOn-ArK
+  * @version V1.0
+  * @date    June_2021
+  * @brief   Testing Template
+  ******************************************************************************
+*/
+
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+			
+
+int main(void)
+{
+	for(;;);
+}
