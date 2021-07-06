0e0cfd40fe823d11d1ceabebd3cf2547b5c3e9d2
CodeOn-ArK <aarya.codeeka@gmail.com>
2021-07-06 20:48:17 +0530

    Final Blow

--- 006Btn_TASK_notification/Core/Src/main.c
+++ 006Btn_TASK_notification/Core/Src/main.c
@@ -228,9 +228,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
